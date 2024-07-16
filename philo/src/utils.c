571de4c70e3e2bdf6c55a6f5336647335cc85feb
sonam <sonam.crumiere@icloud.com>
2024-07-16 17:04:07 +0200

    Final

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 17:53:13 by scrumier          #+#    #+#             */
-/*   Updated: 2024/07/16 16:58:16 by scrumier         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:02:46 by scrumier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,19 +56,14 @@ void	clean(t_table *table)
 	i = 0;
 	while (i < table->philo_nbr)
 	{
-		if (&table->forks[i].fork)
-			pthread_mutex_destroy(&table->forks[i].fork);
-		if (&table->philos[i].philo_mtx)
-			pthread_mutex_destroy(&table->philos[i].philo_mtx);
+		pthread_mutex_destroy(&table->forks[i].fork);
+		pthread_mutex_destroy(&table->philos[i].philo_mtx);
 		i++;
 	}
-	if (&table->write_mtx)
-		pthread_mutex_destroy(&table->write_mtx);
-	if (&table->table_mtx)
-		pthread_mutex_destroy(&table->table_mtx);
+	pthread_mutex_destroy(&table->write_mtx);
+	pthread_mutex_destroy(&table->table_mtx);
 	if (table->forks)
 		free(table->forks);
 	if (table->philos)
 		free(table->philos);
 }
-// TODO check that
